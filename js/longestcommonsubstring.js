6862f0ee695527bd1a4fd188e4694918170112dc
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-09-12 20:47:18 +0530

    Initial addition of problems: longest common sub-sequence and sub-string

--- js/longestcommonsubstring.js
+++ js/longestcommonsubstring.js
@@ -1,34 +1,55 @@
 /**
  * Created by santhoshkumar on 12/09/15.
  */
+
 var util = require("./util");
 
-function findlcs(a, b){
-    var lcs = util.createArray(0,a.length,b.length);
+var solveLCS = function(a,b) {
+    var lcs;
+    var max = 0;
 
-    for(var i = 1; i <= a.length; i++){
-        for(var j = 1; j <= b.length; j++){
-            if (a[i-1] === b[j-1]){
-                lcs[i][j] = 1+lcs[i-1][j-1];
-            }else{
-                lcs[i][j] = 0;
+    var BuildMatrix = function() {
+        lcs = util.createArray(0, a.length, b.length);
+        for (var i = 1; i <= a.length; i++) {
+            for (var j = 1; j <= b.length; j++) {
+                if (a[i - 1] === b[j - 1]) {
+                    lcs[i][j] = 1 + lcs[i - 1][j - 1];
+                } else {
+                    lcs[i][j] = 0;
+                }
+                if (lcs[i][j] > max){
+                    max = lcs[i][j];
+                }
             }
         }
-    }
-    return lcs;
-}
+    };
+
+    this.findMax = function(){
+        BuildMatrix();
+        return max;
+    };
+
+    this.getMatrix = function(){
+        return lcs;
+    };
+};
 
 var a = ['s','a','n','t','h','o','s','h'];
-var b = ['s','a','n','2','o','s','h'];
+var b = ['s','a','n','2','o','s'];
+var i;
 
-console.log(a);
-console.log(b);
-var lcs = findlcs(a,b);
+var lcs = new solveLCS(a,b);
+console.log("********** Result **********");
+console.log("Max Sub String length: "+lcs.findMax());
+
+var resultMatrix = lcs.getMatrix();
 
 for(i = 1; i <= a.length; i++) {
-    lcs[i][0] = a[i-1];
+    resultMatrix[i][0] = a[i-1];
 }
-for( i = 1; i <= b.length; i++) {
-    lcs[0][i] = b[i-1];
+for(i = 1; i <= b.length; i++) {
+    resultMatrix[0][i] = b[i-1];
 }
-console.log(lcs);
+
+console.log("********** Matrix **********");
+console.log(resultMatrix);
