c6a7614b35eeefd6ab795346929de74c0d732a7a
Santhosh Kumar <santhosh.prakash@gmail.com>
2015-09-13 12:11:19 +0530

    Implement rod cutting maximize profits.

--- js/rodcuttingmaxmizeprofit.js
+++ js/rodcuttingmaxmizeprofit.js
@@ -18,3 +18,50 @@
  * price    | 3   5   8   9  10  17  17  20
  *
  */
+
+var util = require("./util");
+
+var RodCutting = function(spotCost){
+    var resultMatrix = [];
+
+    this.findAnswer = function (){
+        var i,j;
+        resultMatrix = util.createArray(0,spotCost.length,spotCost.length);
+
+        for(i=1;i<=spotCost.length;i++){
+            for(j=1;j<=spotCost.length;j++){
+                if (j >= i){
+                    resultMatrix[i][j]=Math.max(resultMatrix[i-1][j],spotCost[i-1]+resultMatrix[i][j-i]);
+                }else{
+                    resultMatrix[i][j]=resultMatrix[i-1][j];
+                }
+            }
+        }
+
+        return resultMatrix[spotCost.length][spotCost.length];
+    };
+
+    this.getMatrix = function(){
+        return resultMatrix;
+    };
+
+    this.geCutPositions = function(){
+        var i = spotCost.length,
+            j = spotCost.length,
+            cutPositions = [];
+        while(j !== 0 && i !== 0) {
+            if (resultMatrix[i][ j] !== resultMatrix[i - 1][ j]) {
+                cutPositions.push(spotCost[i - 1]);
+                j -= i;
+            }else{
+                i--;
+            }
+        }
+        return cutPositions;
+    };
+};
+
+var cost = [ 3 ,  5 ,  8 ,  9 , 10 , 17 , 17 , 20];
+var res = new RodCutting(cost);
+console.log("Maximum Value: "+res.findAnswer());
+console.log(res.geCutPositions());
